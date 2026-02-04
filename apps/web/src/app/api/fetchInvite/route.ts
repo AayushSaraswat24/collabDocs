54678a84a210e413e3dc8d6ed1c034d455a1cb15
Aayush <aayushsaraswat33@gmail.com>
2026-02-04 09:28:52 +0530

    fetch invite route

---
+++ apps/web/src/app/api/fetchInvite/route.ts
@@ -0,0 +1,65 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import {prisma,CollaborationInviteStatus} from "@collabdoc/db"
+
+export async function GET(request:NextRequest){
+    try{
+
+        const session=await getServerSession(authOptions);
+
+        if(!session || !session.user.id){
+            return NextResponse.json({
+              success: false,
+              message: "Unauthorized"
+            }, { status: 401 });
+        }
+
+        const invites=await prisma.collaborationInvite.findMany({
+            where:{
+                inviteeId:session.user.id,
+                status:CollaborationInviteStatus.PENDING
+            },
+            select:{
+                id:true,
+                documentId:true,
+                inviterId:true,
+                role:true,
+
+                document:{
+                    select:{
+                        name:true,
+                    }
+                },
+
+                inviter:{
+                    select:{
+                        name:true,
+                        email:true,
+                        image:true,
+                    }
+                }
+            }
+        })
+
+        if(!invites){
+            return NextResponse.json({
+              success: false,
+              message: "No invites found"
+            }, { status: 200 });
+        }
+
+        return NextResponse.json({
+          success: true,
+          message: "Invites fetched successfully",
+          data: invites
+        }, { status: 200 });
+
+    }catch(error){
+        console.error('Error fetching invites:', error);
+        return NextResponse.json({
+          success: false,
+          message: "internal server error"
+        }, { status: 500 });
+    }
+}
